9e047dff024e94e3f0cb5d174fea544d4644878c
gemengran <gemengran@hotmail.com>
2016-03-03 11:10:58 +0800

    add msgBody

---
+++ CollectionTool/src/main/java/com/colletionUtils/Message/MsgBody.java
@@ -0,0 +1,12 @@
+package com.colletionUtils.Message;
+
+import java.io.Serializable;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+public class MsgBody implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+}
